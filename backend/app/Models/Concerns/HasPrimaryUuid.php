eb6e146a43bb553acec0da3a1cba2e48dd9b867b
yehemo <pidokun9999@gmail.com>
2026-06-08 22:13:51 +0700

    Release v1.1.6: fix UUID model composition on production

--- backend/app/Models/Concerns/HasPrimaryUuid.php
+++ backend/app/Models/Concerns/HasPrimaryUuid.php
@@ -8,7 +8,13 @@ trait HasPrimaryUuid
 {
     use HasUuids;
 
-    public $incrementing = false;
+    public function getIncrementing(): bool
+    {
+        return false;
+    }
 
-    protected $keyType = 'string';
+    public function getKeyType(): string
+    {
+        return 'string';
+    }
 }
